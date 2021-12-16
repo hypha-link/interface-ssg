12bbd099272371f3d95da5eb95fa24982eff3cec
ALegendsTale <1750241-ALegendsTale@users.noreply.gitlab.com>
2021-12-16 05:17:02 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+module.exports = {
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/app': { page: '/app' },
+    }
+  },
+}
