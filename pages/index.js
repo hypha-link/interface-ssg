eb947b03dfc61d30a8d5177b503c381eb03c518a
ALegendsTale <1750241-ALegendsTale@users.noreply.gitlab.com>
2021-12-21 05:28:38 -0500

    + noreferrer
    + noreferrer

--- pages/index.js
+++ pages/index.js
@@ -64,20 +64,20 @@ export default function Home() {
       </main>
       <footer className={styles.footer}>
         <ul>
-          <li><a href='https://gitlab.com/hypha-link' target='_blank'>Gitlab</a></li>
-          <li><a href='https://twitter.com/hyphalink' target='_blank'>Twitter</a></li>
-          <li><a href='https://www.youtube.com/channel/UC2lOBy3z83CXh0Ww66qdPOw' target='_blank'>Youtube</a></li>
-          {/* <li><a href='https://medium.com/@hypha' target='_blank'>Medium</a></li> */}
+          <li><a href='https://gitlab.com/hypha-link' target='_blank' rel="noreferrer">Gitlab</a></li>
+          <li><a href='https://twitter.com/hyphalink' target='_blank' rel="noreferrer">Twitter</a></li>
+          <li><a href='https://www.youtube.com/channel/UC2lOBy3z83CXh0Ww66qdPOw' target='_blank' rel="noreferrer">Youtube</a></li>
+          {/* <li><a href='https://medium.com/@hypha' target='_blank' rel="noreferrer">Medium</a></li> */}
           {/* <li><a>Hypha Discussion Server (Future)</a></li> */}
         </ul>
         <ul>
-          <li><a href='#Privacy' target='_blank'>Privacy Policy</a></li>
-          <li><a href='#Terms' target='_blank'>Terms of Use</a></li>
+          <li><a href='#Privacy' target='_blank' rel="noreferrer">Privacy Policy</a></li>
+          <li><a href='#Terms' target='_blank' rel="noreferrer">Terms of Use</a></li>
         </ul>
         <ul>
-          <li><a href='#FAQ' target='_blank'>FAQ</a></li>
-          <li><a href='#Contact' target='_blank'>Contact</a></li>
-          <li><a href='#Support' target='_blank'>Support</a></li>
+          <li><a href='#FAQ' target='_blank' rel="noreferrer">FAQ</a></li>
+          <li><a href='#Contact' target='_blank' rel="noreferrer">Contact</a></li>
+          <li><a href='#Support' target='_blank' rel="noreferrer">Support</a></li>
         </ul>
       </footer>
     </>
