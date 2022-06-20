7fcbeb11454b711eb78c6556f21991ff8c4fa3b3
ALegendsTale <1750241-ALegendsTale@users.noreply.gitlab.com>
2022-06-20 17:29:07 -0400

    Fixed #93 Profile Discovery, Placeholder profile image
    + Fixed #93 Profile discovery requires wallet. Now allows anyone to search on index page
    + Added placeholder profile image to replace robohash profiles
    + Small css changes to ProfileCard

--- components/hooks/useProfile.ts
+++ components/hooks/useProfile.ts
@@ -1,38 +1,33 @@
-import { EthereumAuthProvider, SelfID } from '@self.id/web';
 import { ethers } from 'ethers';
 import { useState } from 'react'
 import useMountEffect from './useMountEffect';
 import { Profile } from '../utils/Types';
+import { Core } from '@self.id/core';
 
 export default function useProfile( address: string ) {
     const [profile, setProfile] = useState<Profile>();
+    const core = new Core({ ceramic: 'testnet-clay' });
 
     //Retrieve the relevant profile
     useMountEffect(() => {
         const getProfile = async () => {
             try{
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const ownAddress = await provider.getSigner().getAddress();
-            const selfId = await SelfID.authenticate({
-                authProvider: new EthereumAuthProvider(window.ethereum, ownAddress),
-                ceramic: 'testnet-clay',
-                connectNetwork: 'testnet-clay',
-            })
-            //Retrieve the DID address associated with this ethereum address
-            const didAddress = await selfId.client.getAccountDID(`${address}@eip155:${provider.network.chainId}`);
-            //Return the basicProfile associated with this DID address
-            setProfile({address: address, ...await selfId.client.get('basicProfile', didAddress)});
+                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                //Retrieve the DID address associated with this ethereum address
+                const didAddress = await core.getAccountDID(`${address}@eip155:${provider?.network?.chainId || '137'}`);
+                //Return the basicProfile associated with this DID address
+                setProfile({address: address, ...await core.get('basicProfile', didAddress)});
             }
             catch(e){
-            console.warn(`There is no DID that exists for ${address}`);
-            console.log(e);
+                console.warn(`There is no DID that exists for ${address}`);
+                console.log(e);
             }
         }
         if(ethers.utils.isAddress(address)){
             getProfile();
         }
         else{
-            console.log('returning null profile')
+            console.log('Null profile');
             setProfile(null);
         }
     }, [address])
